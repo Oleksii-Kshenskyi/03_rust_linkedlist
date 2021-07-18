ed29d1110521d64af6bc2d2df7849502bf1792ec
Oleksii-Kshenskyi <oleksii.kshenskyi@outlook.com>
2021-07-18 14:19:19 +0300

    Implementation of push. Only Display trait impl remains.

--- src/llist.rs
+++ src/llist.rs
@@ -1,33 +1,46 @@
 use std::boxed::Box;
 
 pub struct List<T> {
-    head: Option<Box<Node<T>>>
+    head: Option<Box<Node<T>>>,
 }
 
 #[derive(Clone)]
 struct Node<T> {
     value: T,
-    next: Option<Box<Node<T>>>
+    next: Option<Box<Node<T>>>,
 }
 
-impl<T> List<T> 
-    where T: Clone {
+impl<T> List<T>
+where
+    T: Clone,
+{
     pub fn new() -> Self {
-        List::<T> {
-            head: None
-        }
+        List::<T> { head: None }
     }
 
     pub fn push(&mut self, new_value: T) {
-        let new_elem = Some(
-            Box::new(
-                    Node::<T> {
-                        value: new_value,
-                        next: self.head.clone()
-                    }
-                )
-            );
+        let new_elem = Some(Box::new(Node::<T> {
+            value: new_value,
+            next: self.head.clone(),
+        }));
         self.head = new_elem;
+    }
+
+    pub fn pop(&mut self) -> Option<T> {
+        match &self.head {
+            None => None,
+            Some(thebox) => {
+                let theval = thebox.value.clone();
+
+                let headref = &mut self.head;
+                let mut boxclone = headref.clone().unwrap();
+                let nextref = &mut boxclone.as_mut().next;
+                *headref = nextref.clone();
+                *nextref = None;
+
+                Some(theval)
+            }
+        }
     }
 
     pub fn for_each(&mut self, foreach_func: fn(&mut T)) {
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 // Basic requirements for this list:
-// - This is a singly linked list, meaning the only pointer is to the head of the list
+// + This is a singly linked list, meaning the only pointer is to the head of the list
 // - The list should implement Rust's Display trait for println! macro to be able to display it
-// - It should have foreach (like map, apply a function to every item in the list)
-// - It should have push to head
-// - It should have pop from head
-
+// + It should have foreach (like map, apply a function to every item in the list)
+// + It should have push to head
+// + It should have pop from head
 
 mod llist;
 use llist::List;
@@ -14,7 +13,19 @@ fn main() {
     trylist.push(22);
     trylist.push(22644);
     trylist.push(161615451313);
+    trylist.push(1414);
 
-    trylist.for_each(|x| { *x += 1 });
-    trylist.for_each(|x| { println!("ELEM: {}!", x)});
+    trylist.pop();
+    trylist.pop();
+    trylist.pop();
+    trylist.pop();
+    trylist.pop();
+
+    trylist.push(13);
+    trylist.push(44);
+    let val = trylist.pop().unwrap();
+    assert_eq!(val, 44);
+
+    trylist.for_each(|x| *x += 1);
+    trylist.for_each(|x| println!("ELEM: {}!", x));
 }
